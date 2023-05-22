cb750981bfdbbb5fa64d3034678275e504ece581
anaconda <vvduc03@gmail.com>
2023-05-22 23:29:25 +0700

    update

--- Assignment/AddBook.py
+++ Assignment/AddBook.py
@@ -1,15 +1,5 @@
 from main import Book
 
 def AddBook(bid, title, author):
-    """
-    Add information of book in to file and return book information
-    :param bid: id of book (unique)
-    :param title: title of book
-    :param author: author of book
-    :return: node store information about the book
-    """
-    # For file
 
-    # For linked list
-    book = Book(bid, title, author)
     return book
--- Assignment/main.py
+++ Assignment/main.py
@@ -1,7 +1,3 @@
-from AddBook import AddBook
-from DeleteBook import DeleteBook
-
-
 class Book():
     """
     Add a node in linked list
@@ -12,6 +8,18 @@ class Book():
         self.author = author
         self.status = status
         self.next = None
+
+class BorrowBook():
+    def __init__(self, bid, borrower):
+        """
+        Create a node contain bid of book and name of borrower
+        :param bid:
+        :param borrower:
+        """
+        self.bid = bid
+        self.borrower = borrower
+        self.next = None
+
 class Library():
     """
     Is linked list datastructure store information about books
@@ -20,8 +28,17 @@ class Library():
         self.head = None
 
     def AddBook_(self, bid, title, author):
-        """Add book into linked list"""
-        book = AddBook(bid, title, author)
+        """
+        Add information of book in to file and return book information
+        :param bid: id of book (unique)
+        :param title: title of book
+        :param author: author of book
+        :return: node store information about the book
+        """
+        # For file
+
+        # For linked list
+        book = Book(bid, title, author)
         self.head.next = book
 
     def __del__(self, remove_bid):
@@ -51,3 +68,18 @@ class Library():
         if current_book == None:
             print("Bid doesn't exist")
 
+    def BorrowBook(self):
+        borrow_bid = input('Enter book id: ')
+        current_book = self.head
+        while current_book.next != None:
+            if current_book.bid == borrow_bid and current_book.status == 1:
+                print('Book was borrowed')
+                break
+            elif current_book.bid == borrow_bid and current_book.status == 0:
+                current_book.status = 1
+                return current_book
+                break
+
+class BorrowedBook():
+    def __init__(self):
+        self.head = None
